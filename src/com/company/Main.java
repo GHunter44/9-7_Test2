309a3ec9b89ddc586cb84e920eea6095bb187e16
Ghunter44 <GBethune@bridgew.student.edu>
2021-09-07 10:17:37 -0400

    Commit test 4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("This is a test");
+        var reader = new Scanner(System.in);
+        System.out.print("How old are you: ");
+        var age = reader.nextFloat();
+        var dogYears = age*7;
+        System.out.println("Wow you are "+age+ " That is " + dogYears+" in dog years");
     }
 }
